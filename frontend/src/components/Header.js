5c12be5ceb680607378602e9eebcd29f5ac5c811
Kazbekgit <kazbekgit@gmail.com>
2025-01-29 17:22:53 +0300

    install react-bootstrap

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+import { Navbar, Nav, Container } from "react";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+
+const Header = () => {
+  return (
+  <header>Header
+  </header>)
+};
+
+export default Header;
